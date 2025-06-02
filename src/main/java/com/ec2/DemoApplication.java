b01f8de1bf83bcc13b72c28f5ac760b8672fe380
zeenaam82 <zeenaam82@gmail.com>
2025-06-02 12:15:57 +0900

    test first commit

---
+++ src/main/java/com/ec2/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
